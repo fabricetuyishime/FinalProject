29caaeeed62ffb722dde63bcb07448607038dd27
uwaberascovia <uwaberaange143@gmail.com>
2023-12-19 11:48:38 +0200

    All good

---
+++ src/test/java/com/example/parkingmanagementsystemsecured/ParkingManagementSystemSecuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingmanagementsystemsecured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementSystemSecuredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
